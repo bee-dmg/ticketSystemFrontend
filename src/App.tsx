cd2eca3aee239e113b44f4c2873a06612bd7f36f
bee schmersal <brandonschmersal@gmail.com>
2023-01-28 19:42:01 -0500

    cleaning up

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main';
-import { TicketsProvider } from './context/ticketsContext';
 
 function App() {
   const tickets:Array<any>=[];
   return (
-    <TicketsProvider value={[]}>
     <div className="App">
      <Main tickets={tickets} />
     </div>
-    </TicketsProvider>
   )
 }
 
--- src/context/ticketsContext.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-const TicketsContext = React.createContext({})
-export const TicketsProvider = TicketsContext.Provider
-export default TicketsContext
