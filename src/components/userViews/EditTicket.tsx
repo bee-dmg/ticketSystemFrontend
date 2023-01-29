47cac027394823d371259fd64ca2c9ea72aae411
bee schmersal <brandonschmersal@gmail.com>
2023-01-29 13:48:08 -0500

    updates.

--- src/components/userViews/EditTicket.tsx
+++ src/components/userViews/EditTicket.tsx
@@ -14,6 +14,15 @@ let ticket = tickets[parseInt(ticketId.ticketId)];
    
        const [title, setTitle] = useState(ticket.title);
        const [description, setDescription] = useState(ticket.description);
+        const[updateStatus,setUpdateStatus]=useState(ticket.status);
+       enum Status{
+        OPEN="OPEN",
+        READY="READY",
+        IN_PROGRESS="IN_PROGRESS",
+        CLOSED="CLOSED",
+       
+      
+      }
 
         return(
         <div>
@@ -23,9 +32,15 @@ let ticket = tickets[parseInt(ticketId.ticketId)];
         <Form.Label>Ticket Title</Form.Label>
         <Form.Control type="text" placeholder="Enter title" value={title} onChange={(e:any)=>{setTitle(e.target.value); console.log(title);}} />
         <Form.Control as="textarea"  rows={3} placeholder="Enter description" value={description} onChange={(e:any)=>{setDescription(e.target.value); console.log(description);}} />
-        <Form.Control type="select" placeholder="status"></Form.Control>
+        <Form.Select aria-label="Default select example" value={updateStatus} onChange={(e:any)=>{setUpdateStatus(e.target.value)}}>
+      
+      <option value={Status.OPEN}>Open</option>
+      <option value={Status.READY}>Ready</option>
+      <option value={Status.IN_PROGRESS}>In Progress</option>
+      <option value={Status.CLOSED}>Closed</option>
+    </Form.Select>
       </Form.Group>
-        <Link to={`/view/${ticket.ticketNumber}`}><Button onClick={()=>{props.editTicket({ticketNumber:ticket.ticketNumber,title:title,status:"open",date: new Date(), reporter:ticket.reporter,description: description})}}>Submit</Button>
+        <Link to={`/view/${ticket.ticketNumber}`}><Button onClick={()=>{props.editTicket({ticketNumber:ticket.ticketNumber,title:title,status:updateStatus,date: new Date(), reporter:ticket.reporter,description: description})}}>Submit</Button>
         </Link>
         </Form>
         
