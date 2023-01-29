9e88d06bc64db9da0b168fcbff662fedf5c44871
bee schmersal <brandonschmersal@gmail.com>
2023-01-29 06:40:50 -0500

    Reducing redundant code in TicketQueueView

--- src/components/userViews/TicketQueueView.tsx
+++ src/components/userViews/TicketQueueView.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button, Card } from 'react-bootstrap';
 function TicketQueueView(props:{tickets:any}) { 
  
    const [tickets, setTickets] = useState(props.tickets);
     const [sortPref, setSortPref]= useState("dateUp");
+let date = tickets.sort((a:any, b:any) => a.date > b.date ? 1 : -1);
 
-    if(sortPref==="dateUp"){
-        console.log(sortPref);
-        console.log(tickets);
+if(sortPref==="dateDown"){
+  
+    date = tickets.sort((a:any, b:any) => a.date > b.date ? -1 : 1);
+
+}
+
+    useEffect(()=>{
+        setTickets(date);
+    },[])
+    if(date.length>0){
 return(
     <><div>Sort By: <Button onClick={()=>setSortPref("dateUp")}>Newest First</Button> / <Button onClick={()=>setSortPref("dateDown")}>Oldest First</Button> </div><div className="row"> <div className="col-3">Title</div></div>{tickets.map((ticket:any)=><div className="row"><div className="col-3">{ticket.ticketNumber}</div><div className="col-3">Title:{ticket.title}</div><div className="col-3">Status:{ticket.status}</div><div className="col-3">Reporter:{ticket.reporter}</div><div className="col-3">Date:{ticket.date.toDateString()} {ticket.date.getHours()}:{ticket.date.getMinutes()}:{ticket.date.getSeconds()}</div><div>Description:{ticket.description}</div>
     
-    </div>).sort((a:any, b:any) => a.date > b.date ? 1 : -1)}</>
+    </div>)}</>
 )}
-if(sortPref==="dateDown"){
-    console.log(tickets);
-    console.log(sortPref);
-    return(
-        <><div>Sort By: <Button onClick={()=>setSortPref("dateUp")}>Newest First</Button> / <Button onClick={()=>setSortPref("dateDown")}>Oldest First</Button> </div>{tickets.map((ticket:any)=><div><div>Title:{ticket.title}</div><div>Status:{ticket.status}</div><div>Reporter:{ticket.reporter}</div><div>Date:{ticket.date.toDateString()} {ticket.date.getHours()}:{ticket.date.getMinutes()}:{ticket.date.getSeconds()}</div><div>Description:{ticket.description}</div>
-        
-        </div>).sort((a:any, b:any) => a.date > b.date ? -1 : 1)}</>
-    )}
-    return(
-        <div>Hello</div>
-    )
+else{
+    return(<div> Hello</div>)
+}
 }
 
 
