08476bf834ac4b4722ce7a3ad438d9e83d9f299f
bee schmersal <brandonschmersal@gmail.com>
2023-01-29 21:50:56 -0500

    edit page

--- src/components/userViews/TicketQueueView.tsx
+++ src/components/userViews/TicketQueueView.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import TicketType from '../../interfaces/TicketInterface';
+import StatusSelector from './StatusSelector';
 function TicketQueueView(props:{tickets:Array<TicketType>}) { 
  
    const [tickets, setTickets] = useState(props.tickets);
@@ -20,7 +21,7 @@ if(sortPref==="dateDown"){
     },[])
     if(date.length>0){
 return(
-    <><div>Sort By: <Button onClick={()=>setSortPref("dateUp")}>Oldest First</Button> / <Button onClick={()=>setSortPref("dateDown")}>Newest First</Button> </div><div className="row"> <div className="col-3">Title</div></div>{tickets.map((ticket:TicketType)=><div className="row"><div className="col-3"><Link to={`/view/${ticket.ticketNumber}`}>{ticket.ticketNumber}</Link></div><div className="col-3">Title:{ticket.title}</div><div className="col-3">Status:{ticket.status.toLowerCase()}</div><div className="col-3">Reporter:{ticket.reporter}</div><div className="col-3">Date:{ticket.date.toDateString()} {ticket.date.getHours()}:{ticket.date.getMinutes()}:{ticket.date.getSeconds()}</div><div>Description:{ticket.description}</div>
+    <><div>Sort By: <Button onClick={()=>setSortPref("dateUp")}>Oldest First</Button> / <Button onClick={()=>setSortPref("dateDown")}>Newest First</Button> </div><div className="row"> <div className="col-3">Title</div></div>{tickets.map((ticket:TicketType)=><div className="row"><div className="col-3"><Link to={`/view/${ticket.ticketNumber}`}>{ticket.ticketNumber}</Link></div><div className="col-3">Title:{ticket.title}</div><div><StatusSelector status={ticket.status} setStatus={"pizza"}/></div><div className="col-3">Status:{ticket.status.toLowerCase()}</div><div className="col-3">Reporter:{ticket.reporter}</div><div className="col-3">Date:{ticket.date.toDateString()} {ticket.date.getHours()}:{ticket.date.getMinutes()}:{ticket.date.getSeconds()}</div><div>Description:{ticket.description}</div>
     
     </div>)}</>
 )}
