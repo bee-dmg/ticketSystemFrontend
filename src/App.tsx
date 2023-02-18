624b89dd0d817523e0c9f3ecd4c614c0496d32bf
bee schmersal <brandon.schmersal@gmail.com>
2023-02-18 12:16:47 -0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import Main from './components/Main';
 import TicketType from './interfaces/TicketInterface';
 import Status from './interfaces/StatusEnum';
 function App() { 
- 
+  fetch('http://localhost:8080/api/tickets', )
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
   const tickets:Array<TicketType>=[];
   return (
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,10 +4,12 @@ import UserViewRouter from './UserViewRouter';
 import TicketType from '../interfaces/TicketInterface';
 import Status from '../interfaces/StatusEnum';
 function Main(props: any) {
+  
 
   const tickets = props.tickets;
 
   function newTicket(ticket: TicketType) {
+    
     tickets.push(ticket);
   }
   function editTicket(ticket: TicketType) {
