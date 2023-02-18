0dd13bdf30f6105bacf742bc865a98716e83964d
bee schmersal <brandon.schmersal@gmail.com>
2023-02-18 13:20:11 -0500

    getting there

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main';
 import TicketType from './interfaces/TicketInterface';
 import Status from './interfaces/StatusEnum';
 function App() { 
-  fetch('http://localhost:8080/api/tickets', )
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+  const [apiTickets, setApiTickets] = useState();
 
-  const tickets:Array<TicketType>=[];
+useEffect(()=>{  fetch('http://localhost:8080/api/tickets')
+.then((response) => response.json())
+.then((data) => setApiTickets(data));},[]);
+  let tickets:Array<TicketType>=[];
+if(apiTickets)
+   console.log(apiTickets)
+  
   return (
     <div className="App">
      <Main tickets={tickets} />
